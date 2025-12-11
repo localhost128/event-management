fcb1f07adb5f17aa06200e0711983f0159086913
Serhii Chukin <serhiichukin@gmil.com>
2025-12-11 01:29:04 +0100

    feet: add users authentication

--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+
+class UserRetriveView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
