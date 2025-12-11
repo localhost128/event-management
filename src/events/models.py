b740ade4026c32619e4391f8d69544b15097343c
Serhii Chukin <serhiichukin@gmil.com>
2025-12-11 03:06:22 +0100

    feet: add events model

--- src/events/models.py
+++ src/events/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Event(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    date = models.DateField()
+    location = models.CharField(max_length=100)
+    organizer = models.ForeignKey(User, related_name="events", on_delete=models.PROTECT)
